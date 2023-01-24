4e58ee4759aae1e33fec935281d094cd038e9272
Martin Blomberg <martin.blomberg@outlook.com>
2023-01-24 10:21:04 +0100

    Add commonCount for Empty arrays

--- src/main/java/org/example/calculator/Calculator.java
+++ src/main/java/org/example/calculator/Calculator.java
@@ -5,4 +5,8 @@ public class Calculator {
     public static int test(int i, int i1) {
         return i + i1;
     }
+
+    public static int commonCount(int[] array1, int[] array2) {
+        return 0;
+    }
 }
