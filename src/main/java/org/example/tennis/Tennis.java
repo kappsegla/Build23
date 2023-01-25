e2d424733e8236297e900f2fa7f1c6d8719fa48d
Martin Blomberg <martin.blomberg@outlook.com>
2023-01-25 10:53:21 +0100

    Add third test for Tennis

--- src/main/java/org/example/tennis/Tennis.java
+++ src/main/java/org/example/tennis/Tennis.java
@@ -1,15 +1,34 @@
 package org.example.tennis;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Tennis {
-    private int score;
+    private Map<String, Integer> scores = new HashMap<>();
+
+    public Tennis() {
+        scores.put("player1", 0);
+        scores.put("player2", 0);
+    }
 
     public String readScore() {
-        if( score > 0)
-            return "15-love";
-        return "love-love";
+        int player1Score = scores.get("player1");
+        int player2Score = scores.get("player2");
+        String scoreAsString = "";
+
+        if (player1Score == 1)
+            scoreAsString += "15";
+        else
+            scoreAsString += "love";
+        scoreAsString += "-";
+        if (player2Score == 1)
+            scoreAsString += "15";
+        else
+            scoreAsString += "love";
+        return scoreAsString;
     }
 
     public void incrementScore(String player) {
-        score++;
+        scores.put(player, scores.get(player) + 1);
     }
 }
--- src/test/java/org/example/tennis/TennisTest.java
+++ src/test/java/org/example/tennis/TennisTest.java
@@ -21,6 +21,16 @@ public class TennisTest {
         assertThat(tennis.readScore()).isEqualTo("15-love");
     }
 
+    @Test
+    void afterPlayersHasWonOneBallEachScoreShouldBe1515() {
+        Tennis tennis = new Tennis();
+
+        tennis.incrementScore("player1");
+        tennis.incrementScore("player2");
+
+        assertThat(tennis.readScore()).isEqualTo("15-15");
+    }
+
 
 
 }
