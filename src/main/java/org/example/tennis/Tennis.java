b64fa9e1b333da60fdade3e34884afeadaeb46fc
Martin Blomberg <martin.blomberg@outlook.com>
2023-01-25 10:32:36 +0100

    Add first test for Tennis

---
+++ src/main/java/org/example/tennis/Tennis.java
@@ -0,0 +1,8 @@
+package org.example.tennis;
+
+public class Tennis {
+
+    public String readScore() {
+        return "love-love";
+    }
+}
---
+++ src/test/java/org/example/tennis/TennisTest.java
@@ -0,0 +1,17 @@
+package org.example.tennis;
+
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class TennisTest {
+
+    @Test
+    void newTennisObjectHasScoreLoveLove() {
+        Tennis tennis = new Tennis();
+
+        assertThat(tennis.readScore()).isEqualTo("love-love");
+    }
+
+
+
+}
