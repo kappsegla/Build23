ccee9faecc453e3c83764fe7e0db5411c40c8f8f
Martin Blomberg <martin.blomberg@outlook.com>
2023-01-27 10:47:04 +0100

    Add Integration tests for App

--- src/test/java/org/example/AppIT.java
+++ src/test/java/org/example/AppIT.java
@@ -1,9 +1,64 @@
 package org.example;
 
+import org.assertj.core.util.Arrays;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Locale;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppIT {
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    private final InputStream stdin = System.in;
+    private final Locale locale = Locale.getDefault();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+        Locale.setDefault(Locale.ENGLISH);
+    }
+
+    @AfterEach
+    void cleanUp() {
+        System.setOut(standardOut);
+        System.setIn(stdin);
+        Locale.setDefault(locale);
+    }
+
+    @Test
+    void invalidOperator() {
+        String data = "%\n1,0\n2,0\n";
+
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+
+        App.main(Arrays.array(""));
+
+        assertThat(outputStreamCaptor.toString()).contains("Invalid operator!");
+    }
+
+    @Test
+    void additionOperator() {
+        String lineSeparator = System.lineSeparator();
+        String data = """
+                      +
+                      1.0
+                      2.0
+                """;
+
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+
+        App.main(Arrays.array(""));
+
+        var result = outputStreamCaptor.toString().split(lineSeparator);
+
+        assertThat(result).contains("1.0 + 2.0 = 3.0");
+    }
 
 }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,11 +1,18 @@
 package org.example;
 
 import nl.jqno.equalsverifier.EqualsVerifier;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
+
+    @Test
+    @Tag("integration")
+    void integrationTestForApp() {
+
+    }
 
 }
