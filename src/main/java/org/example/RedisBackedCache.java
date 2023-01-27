ebaf26847d9522c6b25d51fa94d1c314fdffeda9
Martin Blomberg <martin.blomberg@outlook.com>
2023-01-27 11:40:45 +0100

    Prepare IT test for Redis backed cache

---
+++ src/main/java/org/example/RedisBackedCache.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class RedisBackedCache {
+    public RedisBackedCache(String localhost, int port) {
+
+    }
+
+    public void put(String key, String value) {
+        //Add code to connect to database and add key value
+    }
+
+    public String get(String key) {
+        //Add code to connect to database and read value for key
+        return null;
+    }
+}
---
+++ src/test/java/org/example/RedisInDockerExampleIT.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@Testcontainers
+public class RedisInDockerExampleIT {
+    private RedisBackedCache underTest;
+
+    @Container
+    public GenericContainer redis = new GenericContainer(DockerImageName.parse("redis:5.0.3-alpine"))
+            .withExposedPorts(6379);
+
+    @BeforeEach
+    public void setUp() {
+        // Assume that we have Redis running locally?
+        underTest = new RedisBackedCache("localhost", 6379);
+    }
+
+    @Test
+    @Disabled("Code is not implemented yet")
+    public void testSimplePutAndGet() {
+        underTest.put("test", "example");
+
+        String retrieved = underTest.get("test");
+        assertThat(retrieved).isEqualTo("example");
+    }
+}
