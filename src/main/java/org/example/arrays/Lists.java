4eb9d636d88b59a0e108a537f8c9df974f60e2a0
Martin Blomberg <martin.blomberg@outlook.com>
2024-01-17 11:16:03 +0100

    Optimize creation of new list
    Makes the code harder to read and probably no speed improvement.

--- src/main/java/org/example/arrays/Lists.java
+++ src/main/java/org/example/arrays/Lists.java
@@ -5,9 +5,18 @@ import java.util.List;
 public class Lists {
 
     List<Integer> removeNegativeNumbers(List<Integer> numbers) {
+        boolean shouldBeFiltered = false;
+        for (Integer number : numbers) {
+            if (number < 0) {
+                shouldBeFiltered = true;
+                break;
+            }
+        }
         if( numbers.get(0).equals(0))
             throw new RuntimeException();
-        return numbers.stream().filter(n -> n >= 0).sorted().toList();
+        if( shouldBeFiltered )
+            return numbers.stream().filter(n -> n >= 0).sorted().toList();
+        return numbers;
     }
 
     List<Integer> multiplyOddNumbersBy2(List<Integer> numbers) {
