161ba57326a72eeca858a88b272a5f081e536f75
Martin Blomberg <martin.blomberg@outlook.com>
2024-01-19 13:34:37 +0100

    Second player scores

--- src/main/java/org/example/tdd/TennisGame.java
+++ src/main/java/org/example/tdd/TennisGame.java
@@ -2,13 +2,21 @@ package org.example.tdd;
 
 public class TennisGame {
     private int firstPlayerScore;
+    private int secondPlayerScore;
+
     public String getScore() {
-        if(firstPlayerScore == 1)
+        if (firstPlayerScore == 1)
             return "15-Love";
+        if (secondPlayerScore == 1)
+            return "Love-15";
         return "Love-All";
     }
 
     public void updateFirstPlayerScore() {
         firstPlayerScore++;
+    }
+
+    public void updateSecondPlayerScore() {
+        secondPlayerScore++;
     }
 }
