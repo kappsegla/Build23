a539a88ff4c6ea47a2bc985614941fb862a02aec
Martin Blomberg <martin.blomberg@outlook.com>
2024-01-23 11:42:37 +0100

    Refactor tests

--- src/test/java/org/example/testdoubles/CustomerReaderTest.java
+++ src/test/java/org/example/testdoubles/CustomerReaderTest.java
@@ -1,21 +1,35 @@
 package org.example.testdoubles;
 
 import org.example.testdoubles.utils.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class CustomerReaderTest {
+
+    @Mock
+    EntityManager entityManager;
+    @InjectMocks
+    CustomerReader customerReader;
+
+//    @BeforeEach
+//    void beforeEach(){
+//        entityManager = Mockito.mock(EntityManager.class);
+//        customerReader = new CustomerReader(entityManager);
+//    }
 
     @Test
     @DisplayName("Given Non existing customer id then return empty string")
     void givenNonExistingCustomerIdThenReturnEmptyString() {
-        EntityManager entityManager = Mockito.mock(EntityManager.class);
-        CustomerReader customerReader = new CustomerReader(entityManager);
-
         var result = customerReader.findFullName(123L);
 
         assertThat(result).isEmpty();
@@ -24,8 +38,6 @@ class CustomerReaderTest {
     @Test
     @DisplayName("Given existing customer id then return customer first and last name concatenated")
     void givenExistingCustomerIdThenReturnCustomerFirstAndLastNameConcatenated() {
-        EntityManager entityManager = Mockito.mock(EntityManager.class);
-        CustomerReader customerReader = new CustomerReader(entityManager);
         Customer customer = new Customer(1,"Donald","Duck");
         Mockito.when(entityManager.find(1L)).thenReturn(customer);
 
