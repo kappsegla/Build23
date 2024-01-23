518bb40a1bd4eec0fc11b9e80661ddb991a2d853
Martin Blomberg <martin.blomberg@outlook.com>
2024-01-23 11:19:49 +0100

    Add test for non-existing id

--- src/test/java/org/example/testdoubles/CustomerReaderTest.java
+++ src/test/java/org/example/testdoubles/CustomerReaderTest.java
@@ -1,44 +1,23 @@
 package org.example.testdoubles;
 
 import org.example.testdoubles.utils.EntityManager;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 class CustomerReaderTest {
-    CustomerReader customerReader;
-    EntityManager entityManager;
-
-    @BeforeEach
-    void setUp(){
-        entityManager = mock(EntityManager.class);
-        customerReader = new CustomerReader(entityManager);
-    }
-
-
 
     @Test
-    void findFullNameWithValidIdReturnsFullNameOfCustomer() {
-        Customer customer = new Customer(1,"Kalle","Anka");
-        when(entityManager.find(1L)).thenReturn(customer);
+    @DisplayName("Given Non existing customer id then return empty string")
+    void givenNonExistingCustomerIdThenReturnEmptyString() {
+        EntityManager entityManager = Mockito.mock(EntityManager.class);
+        CustomerReader customerReader = new CustomerReader(entityManager);
 
-        String fullName = customerReader.findFullName(1L);
+        var result = customerReader.findFullName(123L);
 
-        assertEquals("Kalle Anka", fullName);
+        assertThat(result).isEmpty();
     }
-
-    @Test
-    void callingFindFullNameWithInvalidIdReturnsEmptyString() {
-        when(entityManager.find(2L)).thenReturn(null);
-
-        String fullName = customerReader.findFullName(2L);
-
-        assertEquals("", fullName);
-    }
-
-
-
 }
