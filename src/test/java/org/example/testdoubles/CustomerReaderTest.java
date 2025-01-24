2643bc7ea00bcb0949436384894a7eab7dfdd131
Martin Blomberg <martin.blomberg@outlook.com>
2025-01-24 11:31:34 +0100

    Use MockitoExtension

--- src/test/java/org/example/testdoubles/CustomerReaderTest.java
+++ src/test/java/org/example/testdoubles/CustomerReaderTest.java
@@ -3,15 +3,22 @@ package org.example.testdoubles;
 import org.example.testdoubles.utils.EntityManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class CustomerReaderTest {
 
+    @Mock
     EntityManager entityManager;
+
+    @InjectMocks
+    CustomerReader customerReader;
 
     @Test
     @DisplayName("Find customer by id returns full name of customer")
@@ -28,9 +35,7 @@ class CustomerReaderTest {
     @DisplayName("Find customer by id using mockito")
     void findCustomerByIdUsingMockito() {
         //EntityManager entityManager = mock(EntityManager.class);
-        when(entityManager.find(1L)).thenReturn(new Customer(1L, "John","Doe"));
-
-        CustomerReader customerReader = new CustomerReader(entityManager);
+        when(entityManager.find(1L)).thenReturn(new Customer(1L, "John", "Doe"));
 
         var fullName = customerReader.findFullName(1L);
 
