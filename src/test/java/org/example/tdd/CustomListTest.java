868a84c6eba3efd1a11101e828a19913f51ac708
Martin Blomberg <martin.blomberg@outlook.com>
2025-01-24 13:16:27 +0100

    First commit of CustomList

---
+++ src/test/java/org/example/tdd/CustomListTest.java
@@ -0,0 +1,20 @@
+package org.example.tdd;
+
+import org.junit.jupiter.api.Test;
+
+public class CustomListTest {
+
+    //Create new CustomList
+    //Check size
+    //Add a string
+    //Remove
+    //Get
+
+    @Test
+    void createCustomListObject() {
+        CustomList customList = new CustomList();
+    }
+
+
+
+}
