5ccd09921d77b47c53eecad5917588232e1d0413
Martin Blomberg <martin.blomberg@outlook.com>
2025-01-30 13:19:42 +0100

    Tennis Kata
    Refactor tests to parameterized test

--- src/test/java/org/example/kata/TennisTest.java
+++ src/test/java/org/example/kata/TennisTest.java
@@ -2,6 +2,8 @@ package org.example.kata;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -9,18 +11,13 @@ public class TennisTest {
 
     Tennis tennis = new Tennis();
 
-    @Test
-    @DisplayName("Both players have zero points")
-    void bothPlayersHaveZeroPoints() {
-        String score = tennis.score(0,0);
-        assertThat(score).isEqualTo("Love - All");
-    }
-
-    @Test
-    @DisplayName("Player one has 1 point and player two has 0 points")
-    void playerOneHas1PointAndPlayerTwoHas0Points() {
-        String score = tennis.score(1,0);
-        assertThat(score).isEqualTo("Fifteen - Love");
+    @ParameterizedTest
+    @CsvSource({
+            "0, 0, 'Love - All'",
+            "1, 0, 'Fifteen - Love'"
+    })
+    void tennisScore(int playerOneScore, int playerTwoScore, String expectedMessage) {
+        assertThat(tennis.score(playerOneScore, playerTwoScore)).isEqualTo(expectedMessage);
     }
 
 }
