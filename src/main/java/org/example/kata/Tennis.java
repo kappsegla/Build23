40c85febeba6df32a1bdf2e2842965129a7a7045
Martin Blomberg <martin.blomberg@outlook.com>
2025-01-30 13:15:18 +0100

    Tennis Kata
    Player one has 1 point

--- src/main/java/org/example/kata/Tennis.java
+++ src/main/java/org/example/kata/Tennis.java
@@ -2,6 +2,8 @@ package org.example.kata;
 
 public class Tennis {
     public String score(int player1Score, int player2Score) {
+        if( player1Score == 1)
+            return "Fifteen - Love";
         return "Love - All";
     }
 }
--- src/test/java/org/example/kata/TennisTest.java
+++ src/test/java/org/example/kata/TennisTest.java
@@ -16,6 +16,11 @@ public class TennisTest {
         assertThat(score).isEqualTo("Love - All");
     }
 
-
+    @Test
+    @DisplayName("Player one has 1 point and player two has 0 points")
+    void playerOneHas1PointAndPlayerTwoHas0Points() {
+        String score = tennis.score(1,0);
+        assertThat(score).isEqualTo("Fifteen - Love");
+    }
 
 }
