9e6f69e466ecc379f6952a1f6cb603ab1f81a880
Martin Blomberg <martin.blomberg@outlook.com>
2025-01-30 13:50:47 +0100

    Tennis Kata
    player one wins

--- src/test/java/org/example/kata/TennisTest.java
+++ src/test/java/org/example/kata/TennisTest.java
@@ -19,7 +19,8 @@ public class TennisTest {
             "1, 0, 'Fifteen - Love'",
             "2, 0, 'Thirty - Love'",
             "3, 0, 'Forty - Love'",
-            "4, 0, 'Player one wins'"
+            "4, 0, 'Player one wins'",
+            "4, 1, 'Player one wins'"
     })
     void tennisScore(int playerOneScore, int playerTwoScore, String expectedMessage) {
         assertThat(tennis.score(playerOneScore, playerTwoScore)).isEqualTo(expectedMessage);
